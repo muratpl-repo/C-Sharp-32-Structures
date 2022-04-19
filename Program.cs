b526ed3733cea88af8938b4e7717d9d5c219a95e
muratpala007 <muratpala007@gmail.com>
2022-04-19 18:38:46 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _77DersStructs
+{
+    struct Kitap
+    {
+        public string Yazar { get; set; }
+        public string kitapAdi;
+        public double fiyat;
+        public Kitap(string kA,double f)
+        {
+            this.Yazar = "Null";
+            this.kitapAdi = kA;
+            this.fiyat = f;
+
+        }
+    }
+
+    // class:  Komplex yapilarda kullanilir. Kalitim (inheritance) var.
+    // struct: Basic yapilarda kulanilir. Kalitim (inheritance) yok.
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kitap k1 = new Kitap("Suc ve Ceza", 20.25);
+
+        
+
+            Console.WriteLine(k1.kitapAdi, k1.fiyat);
+
+
+            Kitap k2; //Struct Ornegi
+            int x;    //Struct Ornegi
+            float y;  //Struct Ornegi
+        }
+    }
+}
